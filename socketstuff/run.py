b10794ad905fc0f22f18ad672e64986393644409
mikemehl <michaelalanmehl@gmail.com>
2018-03-14 10:55:20 -0400

    Made our client server example a single file. Wow, that was easy.

--- socketstuff/run.py
+++ socketstuff/run.py
@@ -1,5 +1,51 @@
 import os
-from subprocess import Popen
+import socket
+import time
+import multiprocessing 
 
-p1 = Popen(['python3', 'server.py'])
-p2 = Popen(['python3', 'client.py'])
+def server():
+        try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                port = 8967
+
+                s.bind(('', port))
+
+                s.listen(5)
+                print("Listening on port " + str(port) + ".")
+
+                c, addr = s.accept()
+
+                print("Connected from " + str(addr) + ".")
+
+                c.send(bytes('Hey whats up?', 'utf-8'))
+
+                c.close()
+        except:
+                print("Server dun goofed.")
+
+def client():
+        try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                port = 8967
+
+                s.connect(('127.0.0.1', port))
+
+                print(s.recv(0xFF))
+
+                s.close()
+        except:
+                print("Client dun goofed.")
+
+def main():
+        p1 = multiprocessing.Process(target=server)
+        p2 = multiprocessing.Process(target=client)
+        p1.start()
+        time.sleep(1)
+        p2.start()
+        
+        p1.join()
+        p2.join()
+
+
+if __name__ == "__main__":
+        main()
