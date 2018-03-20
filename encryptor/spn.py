ec9f588c6f24d2bbd52abe4e6245abfc01b3dd62
mikemehl <michaelalanmehl@gmail.com>
2018-03-20 18:22:32 -0400

    Added multiprocessing versions. May be able to eliminate these and move pool into my file version so we're not constantly restarting processes unnecessarily.

--- encryptor/spn.py
+++ encryptor/spn.py
@@ -1,5 +1,6 @@
 import random
 import pdb
+import multiprocessing as mp
 
 samplebytes = bytes([0x01,0x02,0x03,0x04])
           # 0   1   2   3  4   5   6   7   8   9   10  11  12  13  14  15
@@ -151,3 +152,97 @@ def decrypt(data, key, stages):
         decrypted.append(one_round_dec(data[i:i+4], key_int, stages))
         print(key_int)
     return b''.join(decrypted)
+
+#########################################################
+# one_round_enc_args
+#########################################################
+# Expects: 
+#         * packed data
+# Returns:
+#         * encrypted data
+def one_round_enc_args(packed):
+    return one_round_enc(packed[0], packed[1], packed[2])
+
+#########################################################
+# one_round_dec_args
+#########################################################
+# Expects: 
+#         * packed data
+# Returns:
+#         * encrypted data
+def one_round_dec_args(packed):
+    return one_round_dec(packed[0], packed[1], packed[2])
+
+#########################################################
+# encrypt_multi 
+#########################################################
+# Expects: 
+#         * data as bytes
+#         * key as a list of four bytes 
+#         * stages as an int
+# Returns:
+#         * decrypted data as a bytestring
+def encrypt_multi(data, key, stages):
+    while len(data)%4 != 0:
+        data.append(0x00)
+    key_int = [int(key[i]) for i in range(0,4)]
+    args = []
+    for i in range(0, len(data), 4):
+        newlist = [None]*3
+        newlist[0] = data[i:i+4]
+        newlist[1] = key_int[1:] + key_int[:1]
+        newlist[2] = stages
+        args.append(newlist)
+    #encrypted.append(one_round_enc(data[i:i+4], key_int, stages))
+    pool = mp.Pool(processes=4)
+    encrypted = pool.map(one_round_enc_args, args)
+    return b''.join(encrypted)
+
+#########################################################
+# decrypt_multi 
+#########################################################
+# Expects: 
+#         * data as bytes
+#         * key as a list of four bytes 
+#         * stages as an int
+# Returns:
+#         * decrypted data as a bytestring
+def decrypt_multi(data, key, stages):
+    while len(data)%4 != 0:
+        data.append(0x00)
+    key_int = [int(key[i]) for i in range(0,4)]
+    args = []
+    for i in range(0, len(data), 4):
+        newlist = [None]*3
+        newlist[0] = data[i:i+4]
+        newlist[1] = key_int[1:] + key_int[:1]
+        newlist[2] = stages
+        args.append(newlist)
+    pool = mp.Pool(processes=4)
+    decrypted = pool.map(one_round_dec_args, args)
+    return b''.join(decrypted)
+
+
+#########################################################
+# encrypt_file 
+#########################################################
+def encrypt_file(fileName, outName, key, stages):
+    inFile = open(fileName, 'rb')
+    outFile = open(outName, 'wb')
+    inData = theFile.read(512)
+    outData = []
+    while inData:
+        outFile.write(encrypt_multi(inData, key, stages))
+        inData = inFile.read(512)
+    
+#########################################################
+# decrypt_file 
+#########################################################
+def decrypt_file(fileName, outName, key, stages):
+    inFile = open(fileName, 'rb')
+    outFile = open(outName, 'wb')
+    inData = theFile.read(512)
+    outData = []
+    while inData:
+        outFile.write(decrypt_multi(inData, key, stages))
+        inData = inFile.read(512)
