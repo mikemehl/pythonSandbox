8555086dbe318339de326c42730f0e8646f9b109
mikemehl <michaelalanmehl@gmail.com>
2018-05-13 09:08:25 -0400

    Hey, lets use classes for this guy.

--- numpystuff/imagething/imagechain.py
+++ numpystuff/imagething/imagechain.py
@@ -1,49 +1,97 @@
 import matplotlib.image as img
 import pdb
 import logging
+import progressbar
 import multiprocessing as mp
+from collections import namedtuple
 
 SAMPLE_FILE = 'sample1.jpg'
-logging.basicConfig(filename='log.txt', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s : %(levelname)6s : %(message)s', level=logging.DEBUG)
+UniqueVals = namedtuple('UniqueVals', 'pixels', 'seqs', 'seqlength')
 
-#Returns a numpy 3d matrix of RGB values.
-def getImgData(filename):
-   return img.imread(filename)
+#Class for image operations and data
+#class ImageData#######################################################################################
+class ImageData:
+   def __init__(self, filename):
+      try:
+         self.filename = filename
+         self.data     = self.getImgData(self.filename)
+         self.pixels   = self.imgToPixels(self.data)
+      except:
+         logging.error("Setting up image data failed for " + str(filename))
+      return
 
-#Takes output of getImgData and returns a set of all the unique R, G, and B values.
-#  aka, all values of 0-255 present in R, G, or B.
-#May want to modify this to get unique RBG tuples.
-def getUniqueValues(imgdata):
-   ret = set()
-   size = imgdata.shape
-   for pixel in imgdata.flat:
-      ret.add(pixel)
-   return ret
+   def __init__(self, filename, length):
+      try:
+         self.filename = filename
+         self.data     = self.getImgData(self.filename)
+         self.pixels   = self.imgToPixels(self.data)
+         self.getUniqueData(length)
+      except:
+         logging.error("Setting up image data failed for " + str(filename))
+      return
+
+   def __del__(self):
+         pass
+
+   def __enter__(self):
+         pass
+
+   def __exit__(self):
+         __del__(self)
+         return
+
+   #Returns a numpy 3d matrix of RGB values.
+   def getImgData(self, filename):
+      try:
+         return img.imread(filename)
+      except:
+         logging.error('Unable to open image: ' + filename)
+         return
+
+   #Converts original image data to a list of pixels
+   def imgToPixels(self, imgdata):
+      print("Converting image format...")
+      return [tuple(imgdata.flat[x:x+3]) for x in range(0,len(imgdata.flat),3)]
+
+   #Takes output of imgToPixels and returns a set of all the unique RGB values.
+   def getUniqueValues(self):
+      ret = set()
+      logging.info("Number of pixels: " + str(len(self.pixels)))
+      print("Reading unique pixels...")
+      with progressbar.ProgressBar(max_value = len(self.pixels), redirect_stdout=True) as bar:
+              k = 0
+              for pixel in self.pixels:
+                 ret.add(tuple(pixel))
+                 k += 1
+                 bar.update(k)
+      return ret
    
-#Same as above, but return unique sequences of a certain length.
-#May want to modify this to get unique sequences of RGB tuples.
-def getUniqueSeqs(imgdata, length):
-   ret = set()
-   size = imgdata.shape
-   logging.info('Number of pixels: ' + str(len(imgdata.flat)))
-   for i in range(0, len(imgdata.flat)):
-      if i+length < len(imgdata.flat):
-         ret.add(tuple(imgdata.flat[i:i+length]))
-      else:
-         break
-   return ret
+   #Same as above, but return unique sequences of a certain length.
+   def getUniqueSeqs(self, length):
+      ret = set()
+      logging.info('Number of pixels: ' + str(len(self.pixels)))
+      print("Reading unique sequences...")
+      with progressbar.ProgressBar(max_value = len(self.pixels), redirect_stdout=True) as bar:
+              k = 0
+              for i in range(0, len(self.pixels)):
+                 if i+length < len(self.pixels):
+                    ret.add(tuple(self.pixels[i:i+length]))
+                    k += 1
+                    bar.update(k)
+                 else:
+                    break
+      return ret
 
-def genIndexes(data):
-   ret = dict()
-   for i in range(0, len(data)):
-      ret[str(data)] = i
-   return ret
-
+   def getUniqueData(self, length):
+      self.values = self.getUniqueValues()
+      self.seqs   = self.getUniqueSeqs(length)
+      return
+###################################################################################################
 def main():
-  data = getImgData(SAMPLE_FILE) 
-  vals = getUniqueValues(data)
-  seqs = getUniqueSeqs(data, 50)
-  #print(seqs)
+  img  = ImageData(SAMPLE_FILE, 50) 
+  logging.debug(list(img.values)[5])
+  logging.debug(list(img.seqs)[5])
   return
 
 if __name__ == '__main__':
