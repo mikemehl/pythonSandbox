53223ef92fed8df5767f93176a8e4ca3285efec8
mikemehl <michaelalanmehl@gmail.com>
2018-05-14 09:35:19 -0400

    Fixed MemoryError using memmap. May lead to other problems though.....

--- numpystuff/imagething/MarkovChain.py
+++ numpystuff/imagething/MarkovChain.py
@@ -10,8 +10,11 @@ import numpy as np
 from ImageData import ImageData
 from ImageData import UniqueVals
 from ImageData import SAMPLE_FILE
+import logging
 
-def setupMatrix(xvals, yvals):
+MAT_FILE = 'markovmtx.dat'
+
+def getMatrixDicts(xvals, yvals):
     xindexes = dict() 
     yindexes = dict()
     i = 0
@@ -20,14 +23,22 @@ def setupMatrix(xvals, yvals):
     j = 0
     for val in yvals:
         yindexes[val]=j
-    return np.zeros((len(xvals),len(yvals))), xindexes, yindexes
+    return xindexes, yindexes
+
+#Use the output of getMatrixDicts and passed in matrix to fill out the matrix.
+#Returns number of values processed (matrix is filled with instances, not probability).
+def addValsToMatrix(mat, xi, yi):
+    return
 
 def main():
-    img = ImageData(SAMPLE_FILE, 50)
-    dude = img.getParams()
-    mat, xi, yi = setupMatrix(dude.seqs, dude.pixels)
-    print("Setup matrix.")
-    print(mat[0][0])
+    img    = ImageData(SAMPLE_FILE, 50)
+    dude   = img.getParams()
+    xi, yi = getMatrixDicts(dude.seqs, dude.pixels)
+    logging.debug('X Length: ' + str(len(xi)))
+    logging.debug('Y Length: ' + str(len(yi)))
+    #Oh boy, this thing is huge. Use int8 for your matrix to save space. 
+    #Don't forget to check for overflow later!
+    mat = np.memmap(MAT_FILE, dtype='int8', mode='w+', shape=(len(xi),len(yi)))
     return
 if __name__ == "__main__":
     main()
