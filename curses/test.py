cc6efd9090e1704dd79a4c14a195ac759444310b
mikemehl <michaelalanmehl@gmail.com>
2018-07-28 10:30:55 -0400

    More infrastructure.

--- curses/test.py
+++ curses/test.py
@@ -11,7 +11,8 @@ class GameWorld:
    MAX_X = 80
    MAX_Y = int(MAX_X / 4)
    def __init__(self):
-      self.worldMap = []
+      self.dungeonMap = []
+      self.actorMap = []
       self.actorList = []
       self.itemList = []
       self.initMap()
@@ -23,20 +24,6 @@ class GameWorld:
      amap[5][6] = '*'
      self.worldMap = amap 
 
-#Super Class for representing an actor (i.e. player/monster) in the game world:
-#  *pos - (x,y,z) where z is dungeon depth
-#  *energy - for determining turns
-#  *hp - health points obviously
-#  *strength - basic combat damage stat
-#  *speed - basic combat evade stat
-class Actor:
-   def __init__(self):
-      self.pos = [1,2,3]
-      self.energy = 5
-      self.hp = 10
-      self.strength = 1
-      self.speed = 1
-
 #Class for representing the player's screen view
 class Screen:
    def __init__(self, stdscr):
@@ -44,10 +31,19 @@ class Screen:
       self.yl = GameWorld.MAX_Y 
       self.theScreen = stdscr
 
-   def drawMap(self, amap):
+   def drawDungeon(self, amap):
+      #Draw the terrain
       for j in range(0, GameWorld.MAX_Y):
          self.theScreen.addstr(j, 0, amap[j][0])
       return
+
+   def drawOverlay(self, actors):
+      for actor in actors:
+         actorChar = actor.repr
+         actorPos = actor.pos
+         self.theScreen.addstr(actorPos[1], \
+            actorPos[0], actorChar)
+
    def refreshScreen(self):
       self.theScreen.refresh()
 
@@ -55,14 +51,63 @@ class Screen:
 class DungeonMaster:
    def __init__(self, stdscr):
       curses.curs_set(0)
+      self.energyThreshold = 5
       self.world = GameWorld()
       self.screen = Screen(stdscr)
-      self.screen.drawMap(self.world.worldMap)
+      self.screen.drawDungeon(self.world.worldMap)
+      self.screen.drawOverlay(self.world.actorList)
+      self.screen.drawOverlay(self.world.itemList)
       self.screen.refreshScreen()
       time.sleep(5)
    
    def getInput(self):
       return self.screen.theScreen.getkey()
+
+   def gameLoop(self):
+      while True:
+         #Go through the list of players.
+         actors = self.world.actorList
+         for actor in actors:
+            #Call each ones action addEnergy method.
+            actor.addEnergy()
+            #If it's energy beats the threshold, it gets a turn.
+            if actors.energy > self.energyThreshold:
+               #If it's the player 
+               if isinstance(actor, Player):
+                  self.playerLoop(actor)
+               #Call the actor's action method
+               actor.action(self.world)
+
+   def playerLoop(self, player):
+      assert(isinstance(player, Player))
+      #Update the screen 
+      self.screen.drawDungeon(self.world.worldMap)
+      self.screen.refreshScreen()
+      inputCycle = True
+      #Get user input 
+      while inputCycle:
+         key = self.getInput();
+         #Tell the Player to process the input 
+         inputCycle = actor.processInput(key) #on returning false, next action callback should be setup
+
+#Super Class for representing an actor (i.e. player/monster) in the game world:
+#  *pos - (x,y,z) where z is dungeon depth
+#  *energy - for determining turns
+#  *hp - health points obviously
+#  *strength - basic combat damage stat
+#  *speed - basic combat evade stat
+#  *action - callback for what to do on a turn, takes 
+#     -GameWorld for updating
+#  *addEnergy - callback for how much energy to add
+class Actor:
+   def __init__(self):
+      self.pos = [1,2,3]
+      self.energy = 5
+      self.hp = 10
+      self.strength = 1
+      self.speed = 1
+      self.action = lambda : None
+      self.addEnergy = lambda : None 
 
 def main(stdscr):
    DungeonMaster(stdscr)            
